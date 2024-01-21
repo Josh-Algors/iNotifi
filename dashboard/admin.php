2a8994065c78f6528f1b6b8948bae18cc674cfa0
JoshDev <olukoyajoshua72@gmail.com>
2024-01-21 23:54:51 +0100

    refactor codes

--- dashboard/admin.php
+++ dashboard/admin.php
@@ -1,26 +1,66 @@
-<?php   
+<?php
 session_start();  
 require '../vendor/autoload.php';
 use Twilio\Rest\Client;
-use Endroid\QrCode\QrCode;
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
+// use Cloudinary\Uploader;
+// use Cloudinary\Api;
+use chillerlan\QRCode\{QRCode, QROptions};
+$dotenv = Dotenv\Dotenv::createImmutable("../");
+$dotenv->load();
 
-// Function to generate a unique QR code for a given data
-function generateQRCode($data)
-{
-    $qrCode = new QrCode($data);
-    $qrCode->setSize(300); // Set the size of the QR code image (e.g., 300 pixels)
+// Replace these with your Cloudinary credentials
+// $cloudName = $_SERVER['CLOUDINARY_NAME'];
+// $apiKey = $_SERVER['CLOUDINARY_API_KEY'];
+// $apiSecret = $_SERVER['CLOUDINARY_SECRET_KEY'];
 
-    return $qrCode->writeString();
-}
+// // Replace these values with your Cloudinary credentials
+// \Cloudinary::config(array(
+//   'cloud_name' => $cloudName,
+//   'api_key' => $apiKey,
+//   'api_secret' => $apiSecret,
+//   'url' => ['secure' => true]
+// ));
 
-// Example of how to use the function
-$qrCodeImage = generateQRCode("tuig99999");
+// $cloudinary = new Cloudinary([
+//     'cloud' => [
+//         'cloud_name' => $cloudName,
+//         'api_key' => $apiKey,
+//         'api_secret' => $apiSecret,
+//         'url' => ['secure' => true]
+//     ]
+// ]);
 
-// Display the image (you might want to save it or embed it in an HTML page)
-header("Content-Type: image/png");
-echo $qrCodeImage;
+// function generateQRCode($data)
+// {
+//     $qrcode = new QRCode();
 
+//     // Generate QR code from data
+//     $image = $qrcode->render($data);
 
+//     return $image;
+// }
+
+// //switch envs
+// $base_url = ($_SERVER['APP_ENV'] == "local") ? $_SERVER['LOCAL_URL'] : $_SERVER['LIVE_URL'];
+// $data = $base_url . "ok";
+// $qrCodeImage = generateQRCode($data);
+// echo $qrCodeImage;
+// exit;
+
+// $svgData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $qrCodeImage));
+
+// // Upload the SVG data to Cloudinary
+// $uploadResult = Uploader::upload($svgData, array(
+//     "resource_type" => "image",
+//     "format" => "svg", // Specify the format to make sure it's treated as an SVG
+//     "public_id" => "desired_public_id", // Optionally set a public ID for the file
+// ));
+
+// // Output the Cloudinary URL of the uploaded image
+// echo $uploadResult;
+// exit;
 
 $conn=mysqli_connect('localhost','root','','iNotifi') or die('Could not Connect My Sql:'.mysql_error());
 if(!isset($_SESSION["admin"])){  
@@ -35,67 +75,306 @@ if(isset($_SESSION["admin"]) && !isset($_SESSION["login"])) {
 
 $_SESSION["registered"]="admin";
 
-echo generateQRCode();
-if(isset($_POST['sendnote'])){
-  $dept = $_POST['dept'];
-  $lev = $_POST['lev'];
-  $va = mysqli_query($conn,"select * from student WHERE `department`='$dept' AND `level`='$lev'");
-  $seatno = $_POST['seat'];
-  
-  
-  while($row = mysqli_fetch_array($va)){
-                
-    $seatvals[] = rand(1,$seatno);
-    $surnames[] = $row['matric no'];
+if (isset($_POST['sendnote'])) {
+    function generateQRCode($data)
+    {
+        $qrcode = new QRCode();
+
+        // Generate QR code from data
+        $image = $qrcode->render($data);
+        return $image;
     }
-    sort($seatvals);
-    sort($surnames);
-    $curdate = date("Y-M-d, h:i:sa");
-    for ($y = 0; $y < mysqli_num_rows($va); $y++) {
-      $curseat = $seatvals[$y];
-      $curname = $surnames[$y];
-      $que = mysqli_query($conn,"INSERT INTO `exam`(`name`, `seat`, `date`) VALUES ('$curname','$curseat','$curdate')");
-      $examdetails = "EXAMINATION DETAILS\nCourse Code/Title: " . $_POST['course'] . "\nExamination Venue: " .$_POST['venue'].
-      "\nExamination Date and Time: " .$_POST['examtime']. "\nDuration: " .$_POST['duration']. 
-      "minutes\nDepartment: " .$_POST['dept']."\nLevel: " .$_POST['lev']. "\nExamination Seat: " .$curseat;
-     
-      $ques = mysqli_query($conn,"SELECT  `phone` FROM `student` WHERE `matric no` = '$curname'");
-      $rows = mysqli_fetch_array($ques);
-      $rowss = "+" . $rows['phone'];
 
-      //config for mails 
-      $quest = mysqli_query($conn,"SELECT  `email` FROM `student` WHERE `matric no` = '$curname'");
-      $rowmail = mysqli_fetch_array($quest);
-      $to = $rowmail['email'];
-      $sub = "Examination Info for " .$_POST['course'];
-      $message = $examdetails;
-      $from = "From: iNotifi6@gmail.com";
-      mail($to,$sub,$message,$from);
-          
-      // Your Account SID and Auth Token from twilio.com/console
-      $account_sid = 'AC039121e48fa58c46c42f3f97ea5bb80c';
-      $auth_token = 'ee0460591e3ed7d65746dcd9badcc0e3';
-      // In production, these should be environment variables. E.g.:
-      // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
+    function generateRandomString($length = 10) {
+      $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      $randomString = '';
+  
+      for ($i = 0; $i < $length; $i++) {
+          $randomString .= $characters[rand(0, strlen($characters) - 1)];
+      }
+  
+      return $randomString;
+  }
+    // echo $qrCodeImage;
+    // exit;
+    
+    $dept = $_POST['dept'];
+    $lev = $_POST['lev'];
+    
+    // Fetch students from the specified department and level
+    $studentsQuery = mysqli_query($conn, "SELECT * FROM student WHERE `department`='$dept' AND `level`='$lev'");
+    
+    // Get the total number of seats
+    $seatno = $_POST['seat'];
+    $rowCount = mysqli_num_rows($studentsQuery);
+
+    if($seatno < $rowCount)
+    {
+      echo '<script>alert("More seats needed!")</script>';
+      exit;
+    }
+
+    // Random number for seat allocation
+    $nums = range(1, $seatno);
+    shuffle($nums);
+
+    // Initialize arrays to store seat values and surnames
+    $seatvals = [];
+    $surnames = [];
+
+    while ($row = mysqli_fetch_array($studentsQuery)) {
+      $seatvals[] = array_pop($nums);
+      $surnames[] = $row['id'];
+    }
+
+    // // Sort arrays
+    // sort($seatvals);
+    // sort($surnames);
+
+    $date = date("Y-M-d, h:i:sa");
+    $examdate = $_POST['examtime'];
+    $reference = generateRandomString();
+    $course_name = $_POST['course'];
+    $exam_date = $_POST['examtime'];
+    $exam_duration = $_POST['duration'];
+    $exam_venue = $_POST['venue'];
+    $hall_size= $seatno;
+
+
+    //switch envs
+    $base_url = ($_SERVER['APP_ENV'] == "local") ? $_SERVER['LOCAL_URL'] : $_SERVER['LIVE_URL'];
+    $data = $base_url . '/dashboard/allocate.php?reference=' . $reference;
+    $qrCodeImage = generateQRCode($data);
+    // echo $qrCodeImage;
+    // // exit;
+
+    // Extract the MIME type and base64-encoded data
+    list($mime, $data) = explode(';', $qrCodeImage);
+    list(, $data)      = explode(',', $data);
+
+    // Decode the base64-encoded data
+    $decodedData = base64_decode($data);
+
+    // // Create a temporary file for the SVG data
+    // $tempFile = tempnam(sys_get_temp_dir(), 'cloudinary_svg');
+    // file_put_contents($tempFile, $decodedData);
+
+    // // Upload the SVG file to Cloudinary
+    // $uploadResult = \Cloudinary\Uploader::upload($tempFile, array(
+    //     "resource_type" => "image",
+    //     "format" => "svg",
+    //     "folder" => "uploads" // Optional: specify a folder in Cloudinary
+    // ));
+
+    // echo 'Uploaded SVG URL: ' . $uploadResult['secure_url'];
+    // exit;
+    // Specify the full path to the root folder and generate a unique filename with the correct extension
+    $rootFolder = "../"; // Assuming this script is in the root folder
+    $uniqueFileName = generateRandomString();
+    $svgFilename = $rootFolder . '/input_image_' . $uniqueFileName. '.svg';
+    $pngFilename = $rootFolder . '/output_image_' . $uniqueFileName . '.png';
+
+    // Save the decoded SVG data to a file
+    file_put_contents($svgFilename, $decodedData);
+
+    // Use Inkscape to convert SVG to PNG
+    $svgPath = "/opt/homebrew/bin/inkscape";
+    $command = $svgPath . ' --export-type=png --export-filename=' . $pngFilename . ' ' . $svgFilename;
+    exec($command, $output, $returnCode);
+
+
+
+    // Insert allocation details into the exam table
+    for ($y = 0; $y < count($surnames); $y++) {
+        $curseat = $seatvals[$y];
+        $student_id = $surnames[$y];
+
+        // echo $student_id . ", " . $reference . ", " . $course_name . ", " . $exam_date . ", " . $curseat . ", " . $exam_duration . ", " . $exam_venue . ", " . $hall_size . ", " . $date;
+        // echo gettype($student_id) . ", " . gettype($reference) . ", " . gettype($course_name) . ", " . gettype($exam_date) . ", " . gettype($curseat) . ", " . gettype($exam_duration) . ", " . gettype($exam_venue) . ", " . gettype($hall_size) . ", " . gettype($date);
+        // exit;
+        // echo $student_id;
+        // Insert into exam table
+        $que = mysqli_query($conn, "INSERT INTO `exam`(`student_id`, `reference`, `course_name`, `exam_date`, `seat`, `exam_duration`, `exam_venue`, `hall_size`, `date`) 
+        VALUES ('$student_id','$reference','$course_name', '$exam_date', '$curseat', '$exam_duration', '$exam_venue', '$hall_size', '$date')");
+
+        $examdetails = "EXAMINATION DETAILS\nCourse Code/Title: " . $_POST['course'] . "\nExamination Venue: " . $_POST['venue'] .
+            "\nExamination Date and Time: " . $_POST['examtime'] . "\nDuration: " . $_POST['duration'] .
+            " minutes\nDepartment: " . $_POST['dept'] . "\nLevel: " . $_POST['lev'] . "\nExamination Seat: " . $curseat;
+
+        // Fetch phone number from the student table
+        // $phoneQuery = mysqli_query($conn, "SELECT `phone` FROM `student` WHERE `matric no` = '$student_id'");
+        // $phoneRow = mysqli_fetch_array($phoneQuery);
+        // $phoneNumber = "+" . $phoneRow['phone'];
+
+        // // Send SMS using Twilio
+        // $account_sid = 'AC039121e48fa58c46c42f3f97ea5bb80c';
+        // $auth_token = 'ee0460591e3ed7d65746dcd9badcc0e3';
+        // $twilio_number = "+12065043061";
+
+        // $client = new Client($account_sid, $auth_token);
+        // $client->messages->create(
+        //     $phoneNumber,
+        //     array(
+        //         'from' => $twilio_number,
+        //         'body' => $examdetails
+        //     )
+        // );
+
+        // Fetch email from the student table
+        $emailQuery = mysqli_query($conn, "SELECT `email` FROM `student` WHERE `id` = '$student_id'");
+        $emailRow = mysqli_fetch_array($emailQuery);
+        $to = $emailRow['email'];
+        $subject = "Examination Info for " . $_POST['course'];
+        $message = $examdetails;
+        $from = "From: olukoyajoshua72@gmail.com";
+
+        $firstnameQuery = mysqli_query($conn, "SELECT `first name` FROM `student` WHERE `id` = '$student_id'");
+        $firstnameRow = mysqli_fetch_array($firstnameQuery);
+        $firstname = $firstnameRow['first name'];
+
+
+        
+        // echo 'Image saved as: ' . $pngFilename;
+        // exit;
+
+        // $rootFolder = "../"; // Assuming this script is in the root folder
+        // $filename = $rootFolder . '/decoded_image_' . uniqid() . '.svg';
+
+        // // Save the decoded data to a file
+        // file_put_contents($filename, $decodedData);
+
+        // // Specify the full path for the converted PNG file
+        // $pngFilename = $rootFolder . '/converted_image_' . uniqid() . '.png';
+
+        // // Use ImageMagick to convert SVG to PNG
+        // exec("convert {$filename} {$pngFilename}");
+
+        // echo 'Image saved as: ' . $pngFilename;
+        // exit;
+
+        $transport = new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl');
+        $transport->setUsername('technitedevs@gmail.com');
+        $transport->setPassword('eydrnlsiubowrsdh');
+
+        $mailer = new Swift_Mailer($transport);
+
+        $message = (new Swift_Message('Test Email'))
+            ->setFrom(['technitedevs@gmail.com' => 'Exam Admin'])
+            ->setTo([$to => $firstname])
+            ->setBody(
+              '<html>
+                  <body>
+                      <p>Hello ' . $firstname. ', kindly find the detailed info for your exam.</p>
+                      <p>Course Name: ' . $course_name . '</p>
+                      <p>Exam Date: ' . $exam_date . '</p>
+                      <p>Allocated Seat: ' . $curseat . '</p>
+                      <p>Exam Duration: ' . $exam_duration . '</p>
+                      <p>Exam Venue: ' . $exam_venue . '</p>
+                      <p>Hall Size: ' . $hall_size . '</p>
+                      <p>Date: ' . $date . '</p>
+                      <p>Department: ' . $_POST['dept'] . '</p>
+                      <p>Level: ' . $_POST['lev'] . '</p>
+                      <img src="../output_image_' . $uniqueFileName . ' alt="QR Code">
+                      <p>You can scan the above QR Code to see full details.</p>
+                  </body>
+                </html>
+                ', 'text/html'
+            );
+
+        $imagePath = $pngFilename;
+        $imageData = file_get_contents($imagePath);
+        $image = new Swift_Image($imageData, 'image.png', 'image/png');
+        $message->attach($image);
+
+        $mailer->send($message);
       
-      // A Twilio number you own with SMS capabilities
-      $twilio_number = "+12065043061";
-      
-      $client = new Client($account_sid, $auth_token);
-      $client->messages->create(
-          // Where to send a text message (your cell phone?)
-          $rowss,
-          array(
-              'from' => $twilio_number,
-              'body' => $examdetails
-          )
-      );
+    }
 
-
-            }
-            echo '<script>alert("Info Sent!")</script>';
-
+    echo '<script>alert("Info Sent!")</script>';
 }
+
+
+// if(isset($_POST['sendnote'])){
+
+//   function generateQRCode($data)
+//   {
+//       $qrcode = new QRCode();
+  
+//       // Generate QR code from data
+//       $image = $qrcode->render($data);
+  
+//       return $image;
+//   }
+  
+//   //switch envs
+//   $base_url = ($_SERVER['APP_ENV'] == "local") ? $_SERVER['LOCAL_URL'] : $_SERVER['LIVE_URL'];
+//   $data = $base_url . $query;
+//   $qrCodeImage = generateQRCode($data);
+//   echo $qrCodeImage;
+//   exit;
+//   // echo '<img src="'.$qrCodeImage.'" alt="QR Code" />';
+//   // exit;
+
+//   $random_number = rand(100000, 999999);
+//   $dept = $_POST['dept'];
+//   $lev = $_POST['lev'];
+//   $va = mysqli_query($conn,"select * from student WHERE `department`='$dept' AND `level`='$lev'");
+//   $seatno = $_POST['seat'];
+  
+  
+//   while($row = mysqli_fetch_array($va)){
+                
+//     $seatvals[] = rand(1,$seatno);
+//     $surnames[] = $row['matric no'];
+//     }
+//     sort($seatvals);
+//     sort($surnames);
+//     $curdate = date("Y-M-d, h:i:sa");
+//     for ($y = 0; $y < mysqli_num_rows($va); $y++) {
+//       $curseat = $seatvals[$y];
+//       $curname = $surnames[$y];
+//       $que = mysqli_query($conn,"INSERT INTO `exam`(`name`, `seat`, `date`) VALUES ('$curname','$curseat','$curdate')");
+//       $examdetails = "EXAMINATION DETAILS\nCourse Code/Title: " . $_POST['course'] . "\nExamination Venue: " .$_POST['venue'].
+//       "\nExamination Date and Time: " .$_POST['examtime']. "\nDuration: " .$_POST['duration']. 
+//       "minutes\nDepartment: " .$_POST['dept']."\nLevel: " .$_POST['lev']. "\nExamination Seat: " .$curseat;
+     
+//       $ques = mysqli_query($conn,"SELECT  `phone` FROM `student` WHERE `matric no` = '$curname'");
+//       $rows = mysqli_fetch_array($ques);
+//       $rowss = "+" . $rows['phone'];
+
+//       //config for mails 
+//       $quest = mysqli_query($conn,"SELECT  `email` FROM `student` WHERE `matric no` = '$curname'");
+//       $rowmail = mysqli_fetch_array($quest);
+//       $to = $rowmail['email'];
+//       $sub = "Examination Info for " .$_POST['course'];
+//       $message = $examdetails;
+//       $from = "From: iNotifi6@gmail.com";
+//       mail($to,$sub,$message,$from);
+          
+//       // Your Account SID and Auth Token from twilio.com/console
+//       $account_sid = 'AC039121e48fa58c46c42f3f97ea5bb80c';
+//       $auth_token = 'ee0460591e3ed7d65746dcd9badcc0e3';
+//       // In production, these should be environment variables. E.g.:
+//       // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
+      
+//       // A Twilio number you own with SMS capabilities
+//       $twilio_number = "+12065043061";
+      
+//       $client = new Client($account_sid, $auth_token);
+//       $client->messages->create(
+//           // Where to send a text message (your cell phone?)
+//           $rowss,
+//           array(
+//               'from' => $twilio_number,
+//               'body' => $examdetails
+//           )
+//       );
+
+
+//             }
+//             echo '<script>alert("Info Sent!")</script>';
+
+// }
 if(isset($_POST['sendmessage'])){
   $vals = mysqli_query($conn,"select * from student ");
   $message = $_POST['message'];
@@ -253,11 +532,12 @@ if(isset($_POST['sendmessage'])){
                     
                     <input name="seat" class="form-control" placeholder="Hall Size" type="text" id="seat" required="required">
                   </div>
-	            
+                 
+                      
 	            <div class="form-group captcha-box">
 	                                                <!--<div class="g-recaptcha" data-sitekey="6Lfwu8sUAAAAAGi3hFs-D8F8o2ZLI1mzBA2fIRiS" data-callback="enableBtn"></div>--> 
                                                     <div class="submit">
-		            		<input class="btn btn-primary" style="color:#ffffff; text-align:center;" name="sendnote" id="signInButton1" type="submit" value="Send Notfication!" disabled="">
+		            		<input class="btn btn-primary" style="color:#ffffff; text-align:center;" name="sendnote" id="signInButton1" type="submit" value="Allocate!" disabled="">
 		            	</div>
                         
                        
---
+++ dashboard/allocate.php
@@ -0,0 +1,71 @@
+<?php
+
+$conn=mysqli_connect('localhost','root','','iNotifi') or die('Could not Connect My Sql:'.mysql_error());
+
+// Function to sanitize and validate input
+// function sanitizeInput($data) {
+//     return htmlspecialchars(strip_tags($data));
+// }
+
+// Check if the reference value is present in the URL
+if (isset($_GET['reference']) && !empty($_GET['reference'])) {
+    // Sanitize the reference value
+    $reference = $_GET['reference'];
+
+    // Prepare SQL statement
+    $sql = "SELECT e.id, s.firstname, s.lastname, s.matric_no, s.department, s.level, e.course_name, e.exam_date, e.seat, e.exam_duration, e.exam_venue, e.hall_size
+            FROM exam e
+            JOIN student s ON e.student_id = s.id
+            WHERE e.reference = ?";
+        
+    $stmt = mysqli_stmt_init($conn);
+
+    if ($stmt->prepare($sql)) {
+
+    $stmt->bind_param("s", $reference);
+
+    // Execute the query
+    $stmt->execute();
+
+    // Get the result
+    $result = $stmt->get_result();
+
+    // Check if there are rows returned
+    if ($result->num_rows > 0) {
+        echo '<h2> ALLOCATION TABLE </h2>';
+        echo '<table border="1">';
+        echo '<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Matric No</th><th>Department</th><th>Level</th><th>Course Name</th><th>Exam Date</th><th>Seat</th><th>Exam Duration</th><th>Exam Venue</th><th>Hall Size</th></tr>';
+
+        // Output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo '<tr>';
+            echo '<td>' . $row["id"] . '</td>';
+            echo '<td>' . $row["firstname"] . '</td>';
+            echo '<td>' . $row["lastname"] . '</td>';
+            echo '<td>' . $row["matric_no"] . '</td>';
+            echo '<td>' . $row["department"] . '</td>';
+            echo '<td>' . $row["level"] . '</td>';
+            echo '<td>' . $row["course_name"] . '</td>';
+            echo '<td>' . $row["exam_date"] . '</td>';
+            echo '<td>' . $row["seat"] . '</td>';
+            echo '<td>' . $row["exam_duration"] . '</td>';
+            echo '<td>' . $row["exam_venue"] . '</td>';
+            echo '<td>' . $row["hall_size"] . '</td>';
+            echo '</tr>';
+        }
+
+        echo '</table>';
+    } else {
+        echo 'No records found for the given reference.';
+    }
+
+    // Close the statement
+    $stmt->close();
+} else {
+    echo 'Error preparing statement: ' . $conn->error;
+}
+$conn->close();
+} else {
+    echo 'Reference value not provided in the URL.';
+}
+?>
